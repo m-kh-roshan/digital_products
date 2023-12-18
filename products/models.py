203bae25dd9930123c68c7ecd78717f378a1bdd3
m-kh-roshan <rezaroshan100178@gmail.com>
2023-12-18 16:52:13 +0330

    product migrate

--- products/models.py
+++ products/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
-    parent = models.ForeignKey('self', verbose_name=_('parent', blank=True, null=True, on_delete=models.CASCADE))
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(_('title'),max_length=50)
     decription = models.TextField(_('description'),blank=True)
-    avatar = models.ImageField(_('avatar'),blank=True, upload_to='categories')
-    is_enable = models.BooleanField(_('is enable'),defult=True)
+    avatar = models.ImageField(_('avatar'),blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'),auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'),auto_now=True)
 
@@ -20,7 +20,7 @@ class Category(models.Model):
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
-    is_enable = models.BooleanField(_('is enable'), defult=True)
+    is_enable = models.BooleanField(_('is enable'), default=True)
     categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'),auto_now=True)
@@ -35,7 +35,7 @@ class File(models.Model):
     prduct = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
     title = models.CharField(_('title'), max_length=50)
     file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
-    is_enable = models.BooleanField(_('is enable'),defult=True)
+    is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'),auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'),auto_now=True)
 
