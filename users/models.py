fefd0b36d7def4b1b14642753a0f1f1cc1c61c69
m-kh-roshan <rezaroshan100178@gmail.com>
2023-12-20 02:52:43 +0330

    users app maked

---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+#will fill
